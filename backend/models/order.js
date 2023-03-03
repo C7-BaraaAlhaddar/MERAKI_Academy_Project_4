74a64517a4a771b72ff0b865970b8827e6f06b69
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-03-04 02:34:36 +0300

    orderSchema

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const ordersSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   paymentsMethod: { type: String, required: true },
   shipping: { type: Boolean, required: true },
   successfulPayment: { type: Boolean, required: true },
 });
 
-module.exports = mongoose.model("Order", ordersSchema);
+module.exports = mongoose.model("Order", orderSchema);
