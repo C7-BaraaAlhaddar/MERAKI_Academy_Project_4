77ebec4fd13f6127d1ade301bfc849237be50a14
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-03-03 19:27:01 +0300

    building the database

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  productName: { type: String, required: true, unique: true },
+  price: { type: Number, required: true },
+  brand: { type: String },
+  img: { type: String },
+  description: { type: String },
+  color: { type: String },
+  quantity: { type: Number },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+});
+
+module.exports("Product", productSchema);
