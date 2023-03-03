4b81ba30adc3ab21307a489b57047be24e7700b2
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-03-04 01:34:51 +0300

    deleteUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -121,8 +121,35 @@ const updateUserById = (req, res) => {
     });
 };
 
+// delete a user by id
+const deleteUserById = (req, res) => {
+  const id = req.params.id;
+  userModel
+    .findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The user with id => ${id} not found`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `User deleted`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+};
+
 module.exports = {
   register,
   login,
   updateUserById,
+  deleteUserById,
 };
