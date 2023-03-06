215fec7aa301813cb624edbf456cd299f50b2230
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-03-06 17:53:02 +0300

    working on the register page

--- frontend/src/components/Register/index.jsx
+++ frontend/src/components/Register/index.jsx
@@ -14,6 +14,15 @@ export default function index() {
       {" "}
       <Container>
         <Card style={{ padding: "10px" }}>
+          <Container
+            style={{
+              padding: "10px 50px",
+              marginBottom: "10px",
+            }}
+          >
+            <Card.Title style={{ fontSize: "50px" }}>Sign Up</Card.Title>
+          </Container>
+
           <Form
             onSubmit={(e) => {
               e.preventDefault();
@@ -31,45 +40,31 @@ export default function index() {
                 <Form.Control type="text" placeholder="Last name" />
               </Form.Group>
             </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </Row>
+            <Form.Group className="mb-3" controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formGridPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
 
             <Form.Group className="mb-3" controlId="formGridAddress1">
               <Form.Label>Address</Form.Label>
               <Form.Control placeholder="1234 Main St" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-              <Form.Label>Address 2</Form.Label>
-              <Form.Control placeholder="Apartment, studio, or floor" />
-            </Form.Group>
-
             <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control />
+              <Form.Group as={Col} controlId="formGridAge">
+                <Form.Label>Age</Form.Label>
+                <Form.Control placeholder="Age" />
               </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                  <option>Choose...</option>
-                  <option>...</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control />
+              <Form.Group as={Col} controlId="formGridPhoneNumber">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control placeholder="012 - 3456789" />
               </Form.Group>
             </Row>
 
