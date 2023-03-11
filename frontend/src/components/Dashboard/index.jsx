1c4665e07bac2d3a7fbd4d20856635be83b94827
BaraaAlhaddar <albaraa.alhaddar@gmail.com>
2023-03-11 23:02:09 +0300

    orders page for admin dashboard

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  InputGroup,
+  ListGroup,
+  Card,
+} from "react-bootstrap";
+import { UserContext } from "../UserContext";
+import axios from "axios";
+import { useNavigate, Route, Routes } from "react-router-dom";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import { CloudinaryImage } from "@cloudinary/url-gen";
+import Orders from "../orders";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { userRole } = useContext(UserContext);
+  useEffect(() => {
+    if (userRole !== "admin") {
+      navigate("/");
+    }
+  }, []);
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <div className="row">
+        <div className="col-md-3" style={{ marginBottom: "10px" }}>
+          <Card>
+            <Card.Body>
+              <ListGroup>
+                <ListGroup.Item
+                  onClick={(e) => navigate("/dashboard")}
+                  className="list-filter"
+                >
+                  Orders
+                </ListGroup.Item>
+                <ListGroup.Item className="list-filter">Users</ListGroup.Item>
+                <ListGroup.Item className="list-filter">
+                  Products
+                </ListGroup.Item>
+                <ListGroup.Item className="list-filter">
+                  Add a new Product
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </div>{" "}
+        <div className="col-md">
+          {" "}
+          <Routes>
+            <Route path="/" element={<Orders />} />
+          </Routes>
+        </div>
+      </div>
+    </Container>
+  );
 }
---
+++ frontend/src/components/orders/index.jsx
@@ -0,0 +1,87 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, Button, ListGroup } from "react-bootstrap";
+import { UserContext } from "../UserContext";
+import axios from "axios";
+export default function Orders() {
+  const { token } = useContext(UserContext);
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/order`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((result) => {
+        setOrders(result.data.orders);
+        console.log(result.data.orders);
+      })
+      .catch((error) => console.log(error.response.data.message));
+  }, []);
+  return (
+    <div>
+      {orders.length > 0 ? (
+        <>
+          {orders.map((order, i) => {
+            return (
+              <Card key={order._id} style={{ marginBottom: "10px" }}>
+                <Card.Header>
+                  <Card.Text>{`Order #${i + 1}`}</Card.Text>
+                </Card.Header>
+                <Card.Body>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <strong>User : </strong>{" "}
+                      {`${order.user.firstName} ${order.user.lastName}`}{" "}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      {" "}
+                      <strong>
+                        Address :{" "}
+                      </strong> {`${order.user.address} `}{" "}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      {" "}
+                      <strong>
+                        Number :{" "}
+                      </strong> {`${order.user.phoneNumber} `}{" "}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <strong>Orders : </strong>{" "}
+                      {order.orders.map((order) => {
+                        return order.label + " /";
+                      })}{" "}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      {" "}
+                      <strong>Total : </strong> {`${order.total} JD`}{" "}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      {" "}
+                      <strong>Payment Method : </strong>{" "}
+                      {`${order.paymentMethod}`}{" "}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      {" "}
+                      <strong>Delivered : </strong>{" "}
+                      {order.shipping ? "Yes" : "Not Yet"}{" "}
+                    </ListGroup.Item>
+                  </ListGroup>
+                  <Button style={{ marginTop: "10px" }}>
+                    Mark as delivered
+                  </Button>
+                  <Button variant="danger" style={{ margin: "10px 0 0 10px" }}>
+                    Delete order
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <div>No Orders Yet</div>
+        </>
+      )}
+    </div>
+  );
+}
